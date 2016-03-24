3c795aea461d5c07b15bd4fc667ab963aa1d866d
evan stewart and patricia hall <student@epicodus.com>
2016-03-23 20:44:49 -0700

    added prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
-function Cash(name, amount, deposit, withdraw) {
-  this.name = name;
+function Cash(amount, deposit, withdraw) {
   this.amount = amount;
   this.deposit = deposit;
   this.withdraw = withdraw;
 }
 
+Cash.prototype.deposit = function(inputDeposit) {
+  this.deposit
+}
 
 $(document).ready(function()  {
   $("form#details").submit(function(event) {
@@ -15,5 +17,18 @@ $(document).ready(function()  {
     $("#showinfo").show();
       $("#showname").text(inputFullName);
       $("#showamount").text(inputAmount);
+
+    $(".firstcolumn").hide();
+
+  $(".transbutton").submit(function(event) {
+
+    var inputDeposit = $("input#depositamount").val();
+    var inputWithdraw = $("input#withdrawamount").val();
+
+    var currentBalance = new Cash(inputAmount, inputDeposit, inputWithdraw)
+
+    $("#showamount").text(currentBalance);
+
+  })
   });
 });
